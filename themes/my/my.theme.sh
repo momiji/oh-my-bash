4a1fd73fce0d1f4981d0da43b2eb92f633d39863
momiji <momiji>
2019-10-02 00:14:31 +0200

    Update my.theme.sh

--- themes/my/my.theme.sh
+++ themes/my/my.theme.sh
@@ -1,85 +1,72 @@
+_orange="\[\033[48;5;202m\]"
+_green="\[\033[48;5;35m\]"
+_blue="\[\033[48;5;25m\]"
+_purple="\[\033[48;5;92m\]"
+_red="\[\033[48;5;52m\]"
+_gray="\[\033[48;5;240m\]"
+_violet="\[\033[48;5;197m\]"
+
 THEME_CLOCK_FORMAT="%Y-%m-%d %H:%M:%S"
-THEME_CLOCK_COLOR="${yellow}"
-CLOCK_THEME_PROMPT_PREFIX=""
-CLOCK_THEME_PROMPT_SUFFIX=""
+THEME_CLOCK_COLOR="${_gray}"
+CLOCK_THEME_PROMPT_PREFIX=" "
+CLOCK_THEME_PROMPT_SUFFIX=" "
+
+SCM_THEME_CHAR_PREFIX=""
+SCM_THEME_CHAR_SUFFIX=""
 SCM_THEME_PROMPT_PREFIX=""
 SCM_THEME_PROMPT_SUFFIX=""
+SCM_THEME_PROMPT_DIRTY=""
+SCM_THEME_PROMPT_CLEAN=""
 
-SCM_THEME_PROMPT_DIRTY=" ${bold_red}✗${normal}"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
-SCM_GIT_CHAR="${bold_cyan}±${normal}"
-SCM_SVN_CHAR="${bold_green}⑆${normal}"
-SCM_HG_CHAR="${bold_red}☿${normal}"
+SCM_GIT_CHAR="±"
+SCM_SVN_CHAR="⑆"
+SCM_HG_CHAR="☿"
 
 #Mysql Prompt
 export MYSQL_PS1="(\u@\h) [\d]> "
 
-case $TERM in
-        xterm*)
-        TITLEBAR="\[\033]0;\w\007\]"
-        ;;
-        *)
-        TITLEBAR=""
-        ;;
-esac
-
 PS3=">> "
 
-__my_rvm_ruby_version() {
-    local gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
-  [ "$gemset" != "" ] && gemset="@$gemset"
-    local version=$(echo $MY_RUBY_HOME | awk -F'-' '{print $2}')
-    local full="$version$gemset"
-  [ "$full" != "" ] && echo "[$full]"
-}
-
 __my_venv_prompt() {
-  if [ ! -z "$PROMPT_ENVIRONMENT" ]
+  local p=${PROMPT_ENVIRONMENT:-$VIRTUAL_ENV}
+  if [ ! -z "$p" ]
   then
-    echo "[${yellow}@${PROMPT_ENVIRONMENT}${normal}]"
+    echo "${_violet} ${p##*/} "
   fi
 }
 
 is_vim_shell() {
         if [ ! -z "$VIMRUNTIME" ]
         then
-                echo "[${cyan}vim shell${normal}]"
+                echo "[${_blue} vim shell ${normal}]"
         fi
 }
 
 modern_scm_prompt() {
-        CHAR=$(scm_char)
-        if [ $CHAR = $SCM_NONE_CHAR ]
+        scm_prompt_vars &> /dev/null
+        if [ $SCM_CHAR = $SCM_NONE_CHAR ]
         then
                 return
+        elif [ $SCM_DIRTY -eq 0 ]; then
+                echo "${_blue} ${SCM_CHAR} $(scm_prompt_info) "
         else
-                echo "[$(scm_char)][$(scm_prompt_info)]"
+                echo "${_purple} ${SCM_CHAR} $(scm_prompt_info) "
         fi
 }
 
 prompt() {
-
     local my_ps_status=$?
-    local my_ps_host="${green}\h${normal}";
-    local my_ps_user="\[\033[01;32m\]\u\[\033[00m\]";
-    local my_ps_root="\[\033[01;31m\]\u\[\033[00m\]";
-    local my_ps_path="\[\033[01;36m\]\w\[\033[00m\]";
+    local my_ps_host="${_green} \h ";
+    local my_ps_user="${_orange} \u ";
+    local my_ps_path="${_gray} \w ";
+    local my_ps_prompt="${bold_green}\$ "
 
-    [ $my_ps_status -ne 0 ] && my_ps_status="\[\033[01;31m\]$my_ps_status\[\033[00m\] " || my_ps_status=""
+    [ $my_ps_status -ne 0 ] && my_ps_status="${_red} $my_ps_status " && my_ps_prompt="${bold_red}\$ " || my_ps_status=""
 
     # nice prompt
-    case "`id -u`" in
-        0) PS1="${normal}${TITLEBAR}[$(clock_prompt)${normal}][$my_ps_root][$my_ps_host]$(modern_scm_prompt)$(__my_venv_prompt)[${cyan}\w${normal}]$(is_vim_shell)
-${my_ps_status}$ "
-        ;;
-        *) PS1="${normal}${TITLEBAR}[$(clock_prompt)${normal}][$my_ps_user][$my_ps_host]$(modern_scm_prompt)$(__my_venv_prompt)[${cyan}\w${normal}]$(is_vim_shell)
-${my_ps_status}$ "
-        ;;
-    esac
+    PS1="${normal}${TITLEBAR}$(clock_prompt)$my_ps_user$my_ps_host$(modern_scm_prompt)$(__my_venv_prompt)${my_ps_path}${normal}${is_vim_shell}${my_ps_status}${normal}\n${my_ps_prompt}${normal}"
 }
 
 PS2="> "
-
-
 
 safe_append_prompt_command prompt
