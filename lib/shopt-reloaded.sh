aa12699e4529d028d4797a047ac47ff6b97409cd
momiji <momiji@users.noreply.github.com>
2019-10-01 19:49:40 +0200

    Update shopt-reloaded.sh

--- lib/shopt-reloaded.sh
+++ lib/shopt-reloaded.sh
@@ -1,2 +1,7 @@
-bind -f /etc/inputrc
-[ -f /etc/inputrc.reloaded ] && bind -f /etc/inputrc.reloaded
+bind '"\e[5~": history-search-backward'   # PageUp
+bind '"\e[6~": history-search-forward'    # PageDown
+bind '"\e[A": previous-history'           # Up
+bind '"\e[B": next-history'               # Down
+bind '"\e[3;3~": kill-word'               # Alt-Del
+bind '"\e[3;5~": kill-word'               # Ctrl-Del
+bind '"\C-_": unix-word-rubout'           # Ctrl-Backspace
