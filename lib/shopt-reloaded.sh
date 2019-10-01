cfc9efd38043de24312e3367acb49bbf4410e70b
momiji <momiji@users.noreply.github.com>
2019-10-02 00:41:55 +0200

    Update shopt-reloaded.sh

--- lib/shopt-reloaded.sh
+++ lib/shopt-reloaded.sh
@@ -4,4 +4,4 @@ bind '"\e[A": previous-history'           # Up
 bind '"\e[B": next-history'               # Down
 bind '"\e[3;3~": kill-word'               # Alt-Del
 bind '"\e[3;5~": kill-word'               # Ctrl-Del
-bind '"\C-_": unix-word-rubout'           # Ctrl-Backspace
+bind '"\C-_": backward-kill-word'         # Ctrl-Backspace
